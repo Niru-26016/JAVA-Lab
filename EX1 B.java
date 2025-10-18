a43df44111567db0dd37d548097f76d6da506cf2
Niru-26016 <niru26016@gmail.com>
2025-10-18 16:44:39 +0530

    EX1 B added

--- EX1 B.java
+++ EX1 B.java
@@ -6,17 +6,18 @@ class EX1B {
 
     public static void main(String[] args) {
 
-        int a = 0;
-        int b = 1;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the ");
-        int n = scanner.nextInt();
+        int a = 0, b = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of terms: ");
+        int n = sc.nextInt();
+        System.out.print("Fibonacci Series: ");
         for (int i = 1; i <= n; i++) {
 
-            System.out.println(a + " ");
-            int temp = a;
+            System.out.print(a + " ");
+            int next = a + b;
             a = b;
-            b = temp + b;
+            b = next;
         }
+        sc.close();
     }
 }
